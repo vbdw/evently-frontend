fde6661aa5cdbcf7db58fad9faa2643e9318ca77
vbdw <csmartgamers@gmail.com>
2025-02-18 12:48:25 +0100

    change title of logout pop-up

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -85,7 +85,7 @@ export default function Nav() {
                     <div className="fixed inset-0 bg-black bg-opacity-50" />
                     <div className="fixed inset-0 flex items-center justify-center p-4">
                         <div className="bg-white rounded-lg shadow-xl max-w-sm p-6">
-                            <Dialog.Title className="text-lg font-bold text-gray-900">Confirm Deletion</Dialog.Title>
+                            <Dialog.Title className="text-lg font-bold text-gray-900">Logout Confirmation</Dialog.Title>
                             <Dialog.Description className="mt-2 text-sm text-gray-500">
                                 Are you sure you want to log out?
                             </Dialog.Description>
